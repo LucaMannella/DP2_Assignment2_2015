8f0535f1b75f4a567ce9f5445a3f00358240681c
Luke <luca.mannella@outlook.it>
2015-12-31 00:24:55 +0100

    An utility class was added.

---
+++ src/it/polito/dp2/WF/sol2/util/Utility.java
@@ -0,0 +1,34 @@
+package it.polito.dp2.WF.sol2.util;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import it.polito.dp2.WF.sol2.jaxb.ActionType;
+import it.polito.dp2.WF.sol2.jaxb.Workflow;
+
+public class Utility {
+
+	/**
+	 * This method create a Map of Workflow starting from a Set
+	 * @param workflowsSet
+	 * @return A workflow HashMap that use the workflow's name as key
+	 */
+	public static Map<String, Workflow> buildWFMap(Set<Workflow> workflowsSet) {
+		Map<String, Workflow> workflowsMap = new HashMap<String, Workflow>();
+		for(Workflow wf : workflowsSet) {
+			workflowsMap.put(wf.getName(), wf);
+		}
+		return workflowsMap;
+	}
+	
+	
+	public static Map<String, ActionType> buildWFActionsMap(List<ActionType> actionsList) {
+		Map<String, ActionType> wfActionsMap = new HashMap<String, ActionType>();
+		for(ActionType act : actionsList) {
+			wfActionsMap.put(act.getName(), act);
+		}
+		return wfActionsMap;
+	}
+}
