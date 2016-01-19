1276cc6ae1ecca0e6404e03be766b31cda3cbea5
Luke <luca.mannella@outlook.it>
2016-01-19 15:14:51 +0100

    JavaDoc added.

--- src/it/polito/dp2/WF/sol2/util/Utility.java
+++ src/it/polito/dp2/WF/sol2/util/Utility.java
@@ -12,6 +12,7 @@ public class Utility {
 
 	/**
 	 * This method create a Map of Workflow starting from a Set
+	 * 
 	 * @param workflowsSet
 	 * @return A workflow HashMap that use the workflow's name as key
 	 */
@@ -23,7 +24,13 @@ public class Utility {
 		return workflowsMap;
 	}
 	
-	
+	/**
+	 * This method create a Map of {@link ActionType} starting from a List.
+	 * The name of Action will be the key of the Map.
+	 * 
+	 * @param actionsList
+	 * @return An ActionType HashMap that use the action's name as key
+	 */
 	public static Map<String, ActionType> buildWFActionsMap(List<ActionType> actionsList) {
 		Map<String, ActionType> wfActionsMap = new HashMap<String, ActionType>();
 		for(ActionType act : actionsList) {
@@ -31,4 +38,5 @@ public class Utility {
 		}
 		return wfActionsMap;
 	}
+	
 }
