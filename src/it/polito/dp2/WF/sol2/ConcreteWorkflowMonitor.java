7824032cf5c441a1cdebd56d31c4c78645bb6434
s175637 <s175637@studenti.polito.it>
2016-01-21 16:56:49 +0100

    JAXBContext creation moved inside the unmarshall operation.

--- src/it/polito/dp2/WF/sol2/ConcreteWorkflowMonitor.java
+++ src/it/polito/dp2/WF/sol2/ConcreteWorkflowMonitor.java
@@ -1,6 +1,14 @@
 package it.polito.dp2.WF.sol2;
 
 import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI;
+import it.polito.dp2.WF.Actor;
+import it.polito.dp2.WF.ProcessReader;
+import it.polito.dp2.WF.WorkflowMonitor;
+import it.polito.dp2.WF.WorkflowReader;
+import it.polito.dp2.WF.sol2.jaxb.Actors;
+import it.polito.dp2.WF.sol2.jaxb.Process;
+import it.polito.dp2.WF.sol2.jaxb.Workflow;
+import it.polito.dp2.WF.sol2.jaxb.WorkflowManager;
 
 import java.io.File;
 import java.text.DateFormat;
@@ -16,16 +24,6 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
 import org.xml.sax.SAXException;
-
-import it.polito.dp2.WF.Actor;
-import it.polito.dp2.WF.FactoryConfigurationError;
-import it.polito.dp2.WF.ProcessReader;
-import it.polito.dp2.WF.WorkflowMonitor;
-import it.polito.dp2.WF.WorkflowReader;
-import it.polito.dp2.WF.sol2.jaxb.Actors;
-import it.polito.dp2.WF.sol2.jaxb.Process;
-import it.polito.dp2.WF.sol2.jaxb.Workflow;
-import it.polito.dp2.WF.sol2.jaxb.WorkflowManager;
 
 /**
  * This is a concrete implementation of the interface {@link WorkflowMonitor} based on the JAXB framework.
@@ -43,19 +41,25 @@ public class ConcreteWorkflowMonitor implements WorkflowMonitor {
 	
 	public ConcreteWorkflowMonitor() throws SAXException, JAXBException {
 		
-		JAXBContext jc = JAXBContext.newInstance(PACKAGE);
 		String fileName = System.getProperty("it.polito.dp2.WF.sol2.WorkflowInfo.file");
 		WorkflowManager root = null;
 		
 		try {
-			root = unmarshallDocument(jc, new File(fileName));
-		} catch (SAXException | JAXBException e) {
-			System.err.println("Error during the unmarshalling of the XML document...");
+			root = unmarshallDocument(new File(fileName));
+		}
+		catch (SAXException | JAXBException e) {
+			System.err.println("Error during the unmarshalling of the XML document...\n"
+					+e.getMessage());
 			throw e;
+		}
+		catch (IllegalArgumentException e) {
+			System.err.println("Error during the unmarshalling of the XML document...\n"
+					+e.getMessage());
+			throw new JAXBException(e.getMessage());
 		}
 
 		// - Workflows - //
-		System.out.println("DEBUG - In the document there are "+root.getWorkflow().size()+" workflows");
+		System.out.println("In the document there are "+root.getWorkflow().size()+" workflows");
 		
 		workflows = new HashMap<String, WorkflowReader>();
 		for( Workflow wf : root.getWorkflow() ) {
@@ -67,7 +71,7 @@ public class ConcreteWorkflowMonitor implements WorkflowMonitor {
 			if(wf instanceof ConcreteWorkflowReader)
 				((ConcreteWorkflowReader)wf).setWfsInsideProcessActions(workflows);
 		}
-		System.out.println("DEBUG - "+workflows.size()+" workflows were created.");
+		System.out.println(workflows.size()+" workflows were created.");
 		
 		// - Actors - //	TODO: update this part if you want to manage more departments	
 		System.out.println("DEBUG - In the document there are "+root.getActors().size()+" departments");
@@ -75,16 +79,16 @@ public class ConcreteWorkflowMonitor implements WorkflowMonitor {
 		
 		// this loop is executed just one time in this particular application
 		for( Actors dep : root.getActors() ) {
-			System.out.println("DEBUG - In the department there are "+dep.getActor().size()+" actors");
+			System.out.println("In the department there are "+dep.getActor().size()+" actors");
 			for( Actors.Actor xmlActor : dep.getActor() ) {
 				Actor a = new Actor(xmlActor.getName(), xmlActor.getRole());
 				actors.put(a.getName(), a);
 			}
-			System.out.println("DEBUG - "+actors.size()+" actors were created.");
+			System.out.println(actors.size()+" actors were created.");
 		}
 		
 		// - Processes - //
-		System.out.println("DEBUG - In the document there are "+root.getProcess().size()+" processes");
+		System.out.println("In the document there are "+root.getProcess().size()+" processes");
 		
 		int code = 1;
 		processes = new HashMap<String, ProcessReader>();
@@ -97,7 +101,7 @@ public class ConcreteWorkflowMonitor implements WorkflowMonitor {
 	    	
 	    	code++;
 		}
-		System.out.println("DEBUG - "+processes.size()+" processes were created.");
+		System.out.println(processes.size()+" processes were created.");
 		
 		// set processes inside workflows
 		for( WorkflowReader wf : workflows.values() ) {
@@ -139,7 +143,7 @@ public class ConcreteWorkflowMonitor implements WorkflowMonitor {
 			buf.append("\tNo Processes\n");
 		else {
 			for(ProcessReader pr : processes.values())
-				buf.append(pr.toString()+"\n");
+				buf.append("\t"+pr.toString()+"\n");
 		}
 		buf.append("\n");
 		
@@ -209,17 +213,19 @@ public class ConcreteWorkflowMonitor implements WorkflowMonitor {
 	 * 
 	 * @return The root element of the document (a {@link WorkflowManager} object).
 	 * 
-	 * @throws JAXBException If it is not possible to create the XML file.
+	 * @throws JAXBException If it is not possible to create the {@link JAXBContext} or XML file.
 	 * @throws SAXException If a SAX error occurs during parsing the XML Schema.
 	 */
-	private WorkflowManager unmarshallDocument(JAXBContext jc, File inputFile) throws JAXBException, SAXException {
+	private WorkflowManager unmarshallDocument(File inputFile) throws JAXBException, SAXException, IllegalArgumentException {
+		JAXBContext jc = JAXBContext.newInstance(PACKAGE);
+		/* - creating the XML schema to validate the XML file before read it - */
 		Schema schema;
-		try {	//creating the XML schema to validate the XML file
+		try {
 			schema = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI).newSchema(new File(XSD_NAME));
 		}
 		catch(IllegalArgumentException e) {
 			System.err.println("Error! No implementation of the schema language is available");
-			throw new FactoryConfigurationError("No implementation of the schema language is available.");
+			throw e;
 		}
 		catch(NullPointerException e) {
 			System.err.println("Error! The instance of the schema or the file of the schema is not well created!\n");
